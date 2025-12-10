6a0a54b98e2fbd610555df3377028c655555bae0
denis.l <denis.l@itechpsp.com>
2025-12-10 10:00:27 +0300

    limiter

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 import 'dotenv/config';
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 
 import chatRoutes from './routes/chatRoutes.js';
 
 const app = express();
 app.use(express.json());
+
+const limiter = rateLimit({
+  windowMs: 10 * 1000,
+  max: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+app.use(limiter);
+
 app.use('/chat', chatRoutes);
 
-app.listen(4000, () => {
-  console.log('Server started on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
 });
